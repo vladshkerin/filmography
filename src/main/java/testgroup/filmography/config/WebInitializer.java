e30352198584d2861c0cbcbc7402d7f9933be459
Vladimir Shkerin <vladshkerin@gmail.com>
2019-09-09 08:27:09 +0300

    Create hibernate config

--- src/main/java/testgroup/filmography/config/WebInitializer.java
+++ src/main/java/testgroup/filmography/config/WebInitializer.java
@@ -9,7 +9,7 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{HibernateConfig.class};
     }
 
     @Override
